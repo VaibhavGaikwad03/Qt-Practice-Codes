d8d80d63c812757f5eb5adcf710b46f41bc0379a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-01-05 19:04:06 +0530

    Add files via upload

---
+++ Age/main.cpp
@@ -0,0 +1,31 @@
+#include <QApplication>
+#include <QWidget>
+#include <QSlider>
+#include <QSpinBox>
+#include <QHBoxLayout>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QWidget *window = new QWidget;
+    window->setWindowTitle("Enter your age");
+
+    QSpinBox *spinbox = new QSpinBox;
+    spinbox->setRange(0, 130);
+
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    slider->setRange(0, 130);
+
+    QObject::connect(spinbox, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
+    QObject::connect(slider, SIGNAL(valueChanged(int)), spinbox, SLOT(setValue(int)));
+    slider->setValue(21);
+
+    QHBoxLayout *layout = new QHBoxLayout;
+    layout->addWidget(spinbox);
+    layout->addWidget(slider);
+    window->setLayout(layout);
+
+    window->show();
+
+    return a.exec();
+}
